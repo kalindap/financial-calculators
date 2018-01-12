4b763163932fc1ca1e612b3367f786a87172094e
Kalinda Piper <kalindap@gmail.com>
2018-01-12 14:51:21 -0600

    Changed anonymous function to named function

--- setax.js
+++ setax.js
@@ -1,10 +1,12 @@
 var btn = document.getElementById('calcButton');
 
-btn.onclick = function() {
+function calculateSETax() {
+    //get user inputs
     var year = document.getElementById('year').value;
     var profit = document.getElementById('profit').value;
     var otherEarnings = document.getElementById('other-earnings').value;
 
+    //determine maximum taxable earnings based on year
     var maxTaxableEarnings;
     if (year === '2017') {
         maxTaxableEarnings = 127200;
@@ -12,6 +14,7 @@ btn.onclick = function() {
         maxTaxableEarnings = 128700;
     }
 
+    //perform self-employment tax calculation
     var selfEmploymentTax;
     if (profit * .9235 < 400) {
         selfEmploymentTax = 0;
@@ -23,8 +26,13 @@ btn.onclick = function() {
         selfEmploymentTax = profit * .9235 * .029;
     }
 
+    //format self-employment tax with commas and two decimal places
     selfEmploymentTax = selfEmploymentTax.toLocaleString('en-US', { style: 'decimal', maximumFractionDigits : 2, minimumFractionDigits : 2 });
 
+    //display self-employment tax
     var result = document.getElementById('result');
     result.textContent = 'Your self-employment tax for ' + year + ' is $' + selfEmploymentTax + '.';
+
 }
+
+btn.onclick = calculateSETax;
